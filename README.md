f3f688a7fa60bc9e9e4a97896e9fcdf9225872ff
xalkovich <xalkovich@gmail.com>
2017-10-24 16:23:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-xalk [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> 
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-xalk using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-xalk
+```
+
+Then generate your new project:
+
+```bash
+yo xalk
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+BSD-3-Clause © [kirill]()
+
+
+[npm-image]: https://badge.fury.io/js/generator-xalk.svg
+[npm-url]: https://npmjs.org/package/generator-xalk
+[travis-image]: https://travis-ci.org/xalkovich/generator-xalk.svg?branch=master
+[travis-url]: https://travis-ci.org/xalkovich/generator-xalk
+[daviddm-image]: https://david-dm.org/xalkovich/generator-xalk.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/xalkovich/generator-xalk
+[coveralls-image]: https://coveralls.io/repos/xalkovich/generator-xalk/badge.svg
+[coveralls-url]: https://coveralls.io/r/xalkovich/generator-xalk
---
+++ generators/app/index.js
@@ -0,0 +1,100 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+// const mkdirp = require('mkdirp');
+
+// mkdirp.sync('app/images');
+// mkdirp.sync('app/js');
+// mkdirp.sync('dist');
+
+
+module.exports = class extends Generator {
+  // prompting() {
+  //   return this.prompt([{
+  //     type: 'input',
+  //     name: 'name',
+  //     message: 'Your project name',
+  //     default: this.appname // Default to current folder name
+  //   }]).then((answers) => {
+  //     this.props = answers;
+  //   });
+  // }
+
+
+  writing() {
+    // this.fs.copy(
+    //   this.templatePath('main.css'),
+    //   this.destinationPath('app/css/main.css')
+    // );
+    // this.fs.copy(
+    //   this.templatePath('fonts/*'),
+    //   this.destinationPath('app/fonts')
+    // );
+    this.fs.copy(
+      this.templatePath('img/*'),
+      this.destinationPath('app/img')
+    );
+
+    this.fs.copy(
+      this.templatePath('fonts/*'),
+      this.destinationPath('app/fonts')
+    );
+
+    this.fs.copy(
+      this.templatePath('index.html'),
+      this.destinationPath('app/index.html')
+    );
+
+    this.fs.copy(
+      this.templatePath('index.jade'),
+      this.destinationPath('app/index.jade')
+    );
+    this.fs.copy(
+      this.templatePath('main.js'),
+      this.destinationPath('app/js/main.js')
+    );
+    this.fs.copy(
+      this.templatePath('sass/*'),
+      this.destinationPath('app/sass')
+    );
+    this.fs.copy(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js')
+    );
+    this.fs.copy(
+      this.templatePath('.bowerrc'),
+      this.destinationPath('.bowerrc')
+    );
+    this.fs.copy(
+      this.templatePath('bower.json'),
+      this.destinationPath('bower.json')
+    );
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'), {
+        //name: this.props.name
+        name: this.appname
+      }
+    );
+  }
+
+  installingLodash() {
+    this.npmInstall(['gulp', 'browser-sync', 'gulp-sass', 'gulp-csso', 'gulp-rename', 'bower', 'gulp-jade', 'gulp-jade', 'gulp-autoprefixer','gulp-concat','gulp-uglifyjs','gulp-notify','del','gulp-imagemin','imagemin-pngquant'], {
+      'save-dev': true
+    });
+    this.bowerInstall(['jquery','sass-font-awesome','normalize.sass','animate.css'], {
+        'dev': true
+    });
+  }
+  // installingLodash: function() {
+  //   this.bowerInstall(['lodash'], {
+  //     'dev': true
+  //   });
+  // }
+
+  install() {
+    this.installDependencies();
+  }
+
+}
